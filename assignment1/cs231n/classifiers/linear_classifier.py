f2b2a571146936df7f69e2355950539fe2f464e7
toby <cshwlam@comp.polyu.edu.hk>
2016-04-14 16:41:23 +0800

    finished svm

--- assignment1/cs231n/classifiers/linear_classifier.py
+++ assignment1/cs231n/classifiers/linear_classifier.py
@@ -49,7 +49,10 @@ class LinearClassifier(object):
       # Hint: Use np.random.choice to generate indices. Sampling with         #
       # replacement is faster than sampling without replacement.              #
       #########################################################################
-      pass
+      rIdx = np.random.choice(num_train, batch_size)
+      X_batch = X[rIdx, :]
+      y_batch = y[rIdx]
+      
       #########################################################################
       #                       END OF YOUR CODE                                #
       #########################################################################
@@ -63,7 +66,7 @@ class LinearClassifier(object):
       # TODO:                                                                 #
       # Update the weights using the gradient and the learning rate.          #
       #########################################################################
-      pass
+      self.W += -learning_rate  * grad
       #########################################################################
       #                       END OF YOUR CODE                                #
       #########################################################################
@@ -90,8 +93,9 @@ class LinearClassifier(object):
     ###########################################################################
     # TODO:                                                                   #
     # Implement this method. Store the predicted labels in y_pred.            #
-    ###########################################################################
-    pass
+    ###########################################################################    
+    score  = X.dot(self.W)        
+    y_pred = score.argmax(axis=1)
     ###########################################################################
     #                           END OF YOUR CODE                              #
     ###########################################################################
--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -21,15 +21,40 @@ def softmax_loss_naive(W, X, y, reg):
   """
   # Initialize the loss and gradient to zero.
   loss = 0.0
-  dW = np.zeros_like(W)
-
+  dW = np.zeros_like(W)  
   #############################################################################
   # TODO: Compute the softmax loss and its gradient using explicit loops.     #
   # Store the loss in loss and the gradient in dW. If you are not careful     #
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+  # compute the loss and the gradient
+  num_classes = W.shape[1]
+  num_train = X.shape[0]
+  loss = 0.0
+  for i in xrange(num_train):
+    scores = X[i].dot(W)
+    #shift the values, prevent blowup
+    scores -= np.max(scores)
+    
+    p = np.exp(scores) / np.sum(np.exp(scores)) #normalize    
+
+    loss += -np.log(p[y[i]])
+    
+    for j in xrange(num_classes):
+      if j == y[i]:
+         dW[:, j] += X[i] * (p[j] - 1)
+      else:
+         dW[:, j]  += X[i] * p[j]
+
+  # Right now the loss is a sum over all training examples, but we want it
+  # to be an average instead so we divide by num_train.
+  loss /= num_train
+  dW /= num_train
+
+  # Add regularization to the loss.
+  loss += 0.5 * reg * np.sum(W * W)
+  dW += reg*W
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
